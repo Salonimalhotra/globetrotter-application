1e01d2b5d1c51da34178e5792339370c522688a3
vivek-malviya <141719237+vivek-malviya@users.noreply.github.com>
2025-03-02 07:59:28 +0530

    fix json data

--- src/main/java/org/example/globetrotterapplication/service/DestinationService.java
+++ src/main/java/org/example/globetrotterapplication/service/DestinationService.java
@@ -1,10 +1,14 @@
 package org.example.globetrotterapplication.service;
 
+import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.example.globetrotterapplication.api.model.Destination;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -12,15 +16,11 @@ import java.util.concurrent.ThreadLocalRandom;
 @Service
 public class DestinationService {
 
-
     private final List<Destination> destinations;
 
+    public DestinationService() throws IOException {
 
-    public DestinationService() {
-
-        String jsonInput = "[{\"city\":\"Paris\",\"country\":\"France\",\"clues\":[\"This city is home to a famous tower that sparkles every night.\",\"Known as the 'City of Love' and a hub for fashion and art.\"],\"fun_fact\":[\"The Eiffel Tower was supposed to be dismantled after 20 years but was saved because it was useful for radio transmissions!\",\"Paris has only one stop sign in the entire city—most intersections rely on priority-to-the-right rules.\"],\"trivia\":[\"This city is famous for its croissants and macarons. Bon appétit!\",\"Paris was originally a Roman city called Lutetia.\"]},{\"city\":\"Tokyo\",\"country\":\"Japan\",\"clues\":[\"This city has the busiest pedestrian crossing in the world.\",\"You can visit an entire district dedicated to anime, manga, and gaming.\"],\"fun_fact\":[\"Tokyo was originally a small fishing village called Edo before becoming the bustling capital it is today!\",\"More than 14 million people live in Tokyo, making it one of the most populous cities in the world.\"],\"trivia\":[\"The city has over 160,000 restaurants, more than any other city in the world.\",\"Tokyo’s subway system is so efficient that train delays of just a few minutes come with formal apologies.\"]},{\"city\":\"New York\",\"country\":\"USA\",\"clues\":[\"Home to a green statue gifted by France in the 1800s.\",\"Nicknamed 'The Big Apple' and known for its Broadway theaters.\"],\"fun_fact\":[\"The Statue of Liberty was originally a copper color before oxidizing to its iconic green patina.\",\"Times Square was once called Longacre Square before being renamed in 1904.\"],\"trivia\":[\"New York City has 468 subway stations, making it one of the most complex transit systems in the world.\",\"The Empire State Building has its own zip code: 10118.\"]}]";
-
-
+        String jsonInput = FileUtils.readFileToString(new File("src/main/resources/static/data.json"), StandardCharsets.UTF_8);
         destinations = new ArrayList<>();
         JSONArray destinationsJsonArray = new JSONArray(jsonInput);
 
